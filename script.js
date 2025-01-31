2f030a609f567d1f5990c1dcc050ffd691960b3f
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-01-31 17:34:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species=species;
+	}
+	makeSound(){
+		console.log("The ",this.species," makes a sound");
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
+	bark(){
+		console.log("woof");
+	}
+}
 
-class Cat extends Animal {}
-
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr(){
+		console.log("purr");
+	}
+}
+const myCat=new Cat("Siamese");
+myCat.makeSound();
+myCat.purr();
+const myDog=new Dog("Golden Retriever");
+myDog.makeSound();
+myDog.bark();
 // Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
